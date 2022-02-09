7a01ce7f767bad8bb31c5eb82044c5dc1d87185b
eryner-z <stfn.bgr@gmail.com>
2022-02-09 21:33:48 +0100

    white text when background dark

--- src/modules/background.js
+++ src/modules/background.js
@@ -3,6 +3,15 @@ const { body } = document;
 const colorizeBackground = (weatherDataDescription) => {
   body.className = '';
   body.classList.add(weatherDataDescription.replace(/\s/g, '-'));
+
+  const substringRain = 'rain';
+  if (
+    weatherDataDescription.replace(/\s/g, '-') === 'few-clouds' ||
+    weatherDataDescription.includes(substringRain)
+  ) {
+    document.getElementById('currentWeather').style.color = 'whitesmoke';
+    document.getElementById('forecast').style.color = 'whitesmoke';
+  }
 };
 
 export default colorizeBackground;
